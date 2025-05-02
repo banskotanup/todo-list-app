1eec031c2c8a28bd49ea1b347474eadc1a5f72b0
banskotanup <baskota.anup11@gmail.com>
2025-05-03 01:40:32 +0545

    Enhance task card display: add handling for completed tasks with distinct styling and checkbox input. Update task status logic to differentiate between expired and completed tasks.

--- src/taskCardUi.js
+++ src/taskCardUi.js
@@ -1,11 +1,68 @@
 export const taskCard = (() => {
     function cards (taskId, task, priority, dueDate, remDay) {
-        if (remDay === 'Expired' || remDay === "Completed") {
+        if (remDay === 'Expired') {
             const taskList = document.querySelector(".task-list");
             const div = document.createElement("div");
             div.classList.add("card");
             div.classList.add("expired");
             div.setAttribute("style", "color: #6B7283");
+            const h3 = document.createElement("h3");
+            h3.classList.add("card-task-name");
+            h3.innerHTML = `<span>Tasks:</span> ${task}`;
+            div.appendChild(h3);
+
+            const hr = document.createElement("hr");
+            div.appendChild(hr);
+
+            const div1 = document.createElement("div");
+            div1.classList.add("info");
+
+            const p = document.createElement("p");
+            p.innerHTML = `<span><b>Priority:</b></span> ${priority}`;
+            if (priority === "high") {
+                p.setAttribute("style", "color: orange");
+              } else if (priority === "mid") {
+                p.setAttribute("style", "color: yellow");
+              } else {
+                p.setAttribute("style", "color: red");
+              }
+            div1.appendChild(p);
+
+            const p1 = document.createElement("p");
+            p1.innerHTML = `<span><b>Due Date:</b></span> ${dueDate}`;
+            div1.appendChild(p1);
+
+            const p2 = document.createElement("p");
+            p2.innerHTML = `<span><b>Status:</b></span> ${remDay}`;
+            div1.appendChild(p2);
+
+            div.appendChild(div1);
+
+            const div2 = document.createElement("div");
+            div2.classList.add("checkingInp");
+
+            const input = document.createElement("input");
+            input.type = "checkbox";
+            input.name = "checkTask";
+            input.id = "checkTask";
+            input.setAttribute("disabled", "true");
+            div2.appendChild(input);
+
+            const label = document.createElement("label");
+            label.textContent = "Mark as completed.";
+            div2.appendChild(label);
+            div.appendChild(div2);
+
+            const hr1 = document.createElement("hr");
+            div.appendChild(hr1);
+
+            taskList.appendChild(div);
+        }
+        else if (remDay === "Completed") {
+            const taskList = document.querySelector(".task-list");
+            const div = document.createElement("div");
+            div.classList.add("card");
+            div.setAttribute("style", "color: #14B8A6");
             const h3 = document.createElement("h3");
             h3.classList.add("card-task-name");
             h3.innerHTML = `<span>Tasks:</span> ${task}`;
